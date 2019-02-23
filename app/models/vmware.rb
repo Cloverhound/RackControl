96c038484be98b270c96429d3b474fd5f43bbec4
CHAD STACHOWICZ <chadstachowicz@CHADs-iMac.local>
2019-02-23 15:39:24 -0500

    updates for blog

--- app/models/vmware.rb
+++ app/models/vmware.rb
@@ -3,8 +3,8 @@ class Vmware < ApplicationRecord
 
         vim = RbVmomi::VIM.connect(host: device.vsphere_host, user: device.username, password: device.password, insecure: true)
         root_folder = vim.serviceInstance.content.rootFolder
-        dc = root_folder.childEntity.grep(RbVmomi::VIM::Datacenter).find { |x| x.name == 'Cloverhound' } || fail('datacenter not found')
-        vm = dc.vmFolder.childEntity.grep(RbVmomi::VIM::VirtualMachine).find { |x| x.name == 'vWLC' } || fail('VM not found')
+        dc = root_folder.childEntity.grep(RbVmomi::VIM::Datacenter).find { |x| x.name == device.datacenter_name } || fail('datacenter not found')
+        vm = dc.vmFolder.childEntity.grep(RbVmomi::VIM::VirtualMachine).find { |x| x.name == device.vm_name } || fail('VM not found')
         task = vm.PowerOnVM_Task
         filter = vim.propertyCollector.CreateFilter(
           spec: {
